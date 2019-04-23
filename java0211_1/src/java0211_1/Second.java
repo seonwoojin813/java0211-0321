c2f523eb6e3d0aea4edd59b1e82fb6d71d520e82
503_08 <503_08@DESKTOP-7RBI65N>
2019-04-23 18:54:49 +0900

    0211_1

---
+++ java0211_1/src/java0211_1/Second.java
@@ -0,0 +1,20 @@
+package java0211_1;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// System.out.print 와 println의 차이
+		// \t 는 탭, \n은 줄바꿈
+		System.out.println("안녕하세요. \n반갑습니다.");
+           System.out.println("선우진 입니다");
+           System.out.println("오늘은 " + 1  + " 일차" + " 수업입니다");
+           System.out.println(10);
+           //16진수 10
+           System.out.println(0*10);
+           //실수 출력
+           System.out.println(3.14);  //double 실수
+           System.out.println(3.14f);  //float 실수
+           System.out.println(0.314E1); //double 실수
+	}
+
+}
